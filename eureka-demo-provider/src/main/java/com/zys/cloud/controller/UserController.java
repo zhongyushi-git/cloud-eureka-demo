bbd33e2a75c603cac9c3db3f9fe7190414de564c
zhongyushi-git <1916008067@qq.com>
2020-04-08 20:33:20 +0800

    eureka关闭自我保护和发现

--- eureka-demo-provider/src/main/java/com/zys/cloud/controller/UserController.java
+++ eureka-demo-provider/src/main/java/com/zys/cloud/controller/UserController.java
@@ -3,9 +3,12 @@ package com.zys.cloud.controller;
 import com.zys.cloud.entity.User;
 import com.zys.cloud.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -14,6 +17,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("/get/{id}")
     public User getUser(@PathVariable("id")long id){
@@ -31,6 +37,19 @@ public class UserController {
             map.put("msg","添加失败");
             map.put("code",444);
         }
+        return map;
+    }
+
+    @GetMapping("/discovery")
+    public Object discovery(){
+        //获取注册的服务列表信息
+        List<String> services = discoveryClient.getServices();
+        //根据服务名称获取实例列表信息
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-EUREKA-DEMO-PROVIDER");
+
+        Map<String,Object> map=new HashMap<>();
+        map.put("服务列表",services);
+        map.put("实例列表",instances);
         return map;
     }
 }
